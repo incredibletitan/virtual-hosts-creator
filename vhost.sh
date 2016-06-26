0b49a4a4a8266bea3d83396c10bea46ddf350305
stosbsh <100sbsh@gmail.com>
2016-06-26 18:02:53 +0300

    Added detecting of current file directory in cases of executable file is symlink

--- vhost.sh
+++ vhost.sh
@@ -6,8 +6,14 @@ if [ $# -ne 2 ]; then
 	exit 1;
 fi
 
-#Detect current directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#Detect current file directory.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 hostname=$1
 path_to_host=$2
